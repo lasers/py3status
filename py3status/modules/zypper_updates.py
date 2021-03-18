2b7e0f55c344ce4b526b9bf7052aa6bbf3f10055
lasers <lasers@users.noreply.github.com>
2021-03-17 19:37:35 -0500

    zypper_updates: unspecified changes

--- py3status/modules/zypper_updates.py
+++ py3status/modules/zypper_updates.py
@@ -4,23 +4,28 @@ Display number of pending updates for OpenSUSE Linux.
 Display the system updates counted by zypper lu.
 
 Configuration parameters:
-    cache_timeout: How often we refresh this module in seconds
-        (default 600)
-    format: Display format to use
-        (default 'zypper: {updates}')
+    cache_timeout: refresh interval for this module (default 600)
+    format: Display format to use (default 'UPD [\?color=update {update}]')
+    thresholds: specify color thresholds to use
+        (default [(0, 'good'), (5, 'degraded'), (10, 'bad')])
 
 Format placeholders:
-    {updates} number of pending zypper updates
+    {update} number of pending zypper updates
 
-Color options:
-    color_degraded: Upgrade available
-    color_good: No upgrades needed
+Color thresholds:
+    xxx: print a color based on the value of `xxx` placeholder
 
 @author Ioannis Bonatakis <ybonatakis@suse.com>
 @license BSD
 
 SAMPLE OUTPUT
-{'color': '#FFFF00', 'full_text': 'DNF: 14'}
+{'color': '#00FF00', 'full_text': 'UPD 0'}
+
+degraded
+{'color': '#FFFF00', 'full_text': 'UPD 5'}
+
+bad
+{'color': '#FF0000', 'full_text': 'UPD 10'}
 
 """
 
@@ -34,39 +39,30 @@ class Py3status:
 
     # available configuration parameters
     cache_timeout = 600
-    format = "zypper: {updates}"
+    format = "UPD [\?color=update {update}]"
+    thresholds = [(0, "good"), (5, "degraded"), (10, "bad")]
 
     def post_config_hook(self):
-        self._reg_ex_pkg = re.compile(b"v\\s\\S+", re.M)
-        self._first = True
-        self._updates = 0
+        self.thresholds_init = self.py3.get_color_names_list(self.format)
+        self.reg_ex_pkg = re.compile(b"v\\s\\S+", re.M)
 
     def zypper_updates(self):
-        if self._first:
-            self._first = False
-            response = {
-                "cached_until": self.py3.time_in(0),
-                "full_text": self.py3.safe_format(self.format, {"updates": "?"}),
-            }
-            return response
-
         output, error = subprocess.Popen(
             ["zypper", "lu"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
         ).communicate()
-        updates = len(self._reg_ex_pkg.findall(output))
 
-        if updates == 0:
-            color = self.py3.COLOR_GOOD
-            self._updates = 0
-        else:
-            self._updates = updates
-            color = self.py3.COLOR_DEGRADED
-        response = {
-            "cached_until": self.py3.time_in(self.cache_timeout),
-            "color": color,
-            "full_text": self.py3.safe_format(self.format, {"updates": updates}),
+        zypper_data = {
+            "update": len(self.reg_ex_pkg.findall(output)),
         }
-        return response
+
+        for x in self.thresholds_init:
+            if x in zypper_data:
+                self.py3.threshold_get_color(zypper_data[x], x)
+
+        return {
+            "cached_until": self.py3.time_in(self.cache_timeout),
+            "full_text": self.py3.safe_format(self.format, zypper_data),
+        }
 
 
 if __name__ == "__main__":
